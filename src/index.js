f43c64e37c48ef5064a18940bb0bd86af08463c8
dixitt5 <tilajidixit@gmail.com>
2023-11-03 00:18:39 +0530

    same functions of previous bot done in node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const botRoutes = require('./routes/botRoutes');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
